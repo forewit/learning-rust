1730cd54edf4d415ca68c4da71d321645e52da1d
Anderson <MDAnderson@samaritan.org>
2021-08-25 15:26:58 -0400

    na

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,7 +1,22 @@
 fn main() {
-    let spaces = "   ";
-    let spaces = spaces.len();
-    // spaces is now "3" in this scope
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+    
+    impl IpAddr {
+        // define a function for the enum
+        fn to_string(&self) -> String {
+            match self {
+                IpAddr::V4(a, b, c, d) => format!("{}.{}.{}.{}", a, b, c, d),
+                IpAddr::V6(s) => s.to_string(),
+            }
+        }
+    }
 
-    println!("{}", spaces);
+    let v4 = IpAddr::V4(192, 168, 1, 100);
+    let v6 = IpAddr::V6(String::from("2001:0db8:85a3:0000:0000:8a2e:0370:7334"));
+
+    println!("v4: {}", v4.to_string());
+    println!("v6: {}", v6.to_string());
 }
